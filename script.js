e2809c2f78ea2b60821b6992ba3c054571a6ad90
Pau Rodriguez <32781382+putupau@users.noreply.github.com>
2024-11-07 19:02:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Enables the hamburger menu to open and close when clicked
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
